cf919059303333426928cd0808f052e986f06d66
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-26 21:11:38 -0700

    fix error

--- examples/modules.js
+++ examples/modules.js
@@ -2,8 +2,9 @@ var pull = require('pull-stream')
 var pfs  = require('../')
 var path = require('path')
 
-pull.depthFirst(process.cwd, function (dir) {
+pull.depthFirst(process.cwd(), function (dir) {
   return pfs.readdir(path.resolve(dir, './node_modules'))
+          .pipe(pull.filter())
 })
 .pipe(pull.map(function (e) {
   return path.relative(process.cwd(), e)
