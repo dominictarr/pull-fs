919d21db75f519b0eba74b1fb56fb4bb8b739196
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-10 11:14:52 +1200

    update examples to pull-stream@3

--- examples/find-package-json.js
+++ examples/find-package-json.js
@@ -1,11 +1,13 @@
 var pfs = require('../')
 var pull = require('pull-stream')
 
-pfs.ancestors() //ancestors of cwd
-.pipe(pfs.resolve('./package.json'))
-.pipe(pfs.isFile())
-.pipe(pull.find(function (err, file) {
-  if(err) throw err
-  console.log(file)
-}))
+pull(
+  pfs.ancestors(), //ancestors of cwd
+  pfs.resolve('./package.json'),
+  pfs.isFile(),
+  pull.find(function (err, file) {
+    if(err) throw err
+    console.log(file)
+  })
+)
 
--- examples/modules.js
+++ examples/modules.js
@@ -1,13 +1,20 @@
 var pull = require('pull-stream')
 var pfs  = require('../')
 var path = require('path')
+var DepthFirst = require('pull-traverse').depthFirst
 
-pull.depthFirst(process.cwd(), function (dir) {
-  return pfs.readdir(path.resolve(dir, './node_modules'))
-          .pipe(pull.filter())
-})
-.pipe(pull.map(function (e) {
-  return path.relative(process.cwd(), e)
-}))
-.pipe(pull.drain(console.log))
+pull(
+  DepthFirst(process.cwd(), function (dir) {
+    return pull(
+      pfs.readdir(path.resolve(dir, './node_modules'), null, true),
+      pull.filter()
+    )
+  }),
+  pull.map(function (e) {
+    return path.relative(process.cwd(), e)
+  }),
+  pull.drain(console.log, function (err) {
+    if(err) throw err
+  })
+)
 
